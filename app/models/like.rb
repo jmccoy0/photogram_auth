299f43b0443277e9c0c04263320201186202f661
Jessica McCoy <jmccoy0@users.noreply.github.com>
2017-05-26 08:12:41 -0500

    Through 16 in README

---
+++ app/models/like.rb
@@ -0,0 +1,9 @@
+class Like < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :photo
+
+  validates :user_id, :presence => true, :uniqueness => {:scope => :photo, :message => "already liked this photo"}
+  validates :photo_id, :presence => true
+
+end
