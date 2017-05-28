485f4c8cf9db1d1c6518a2a2ee38b7afe02e5d0a
Jessica McCoy <jmccoy0@users.noreply.github.com>
2017-05-28 14:38:37 -0500

    Working project

--- app/controllers/mylikes_controller.rb
+++ app/controllers/mylikes_controller.rb
@@ -1,6 +1,7 @@
 class MylikesController < ApplicationController
   def show
     @my_likes = current_user.liked_photos
+    @users = User.all
 
     render("mylikes/show.html.erb")
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     # @photos = Photo.where(:photo_id => current_user)
     @photos = current_user.photos
+    @users = User.all
 
     render("users/show.html.erb")
   end
