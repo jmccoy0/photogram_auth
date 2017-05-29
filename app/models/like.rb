163d5f0f6be4935aa2f7b2dfc6a6654ff97aec5f
Jessica McCoy <jmccoy0@users.noreply.github.com>
2017-05-29 17:20:46 -0500

    Nearly there...

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,7 +3,7 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :photo
 
-  validates :user_id, :presence => true, :uniqueness => {:scope => :photo, :message => "already liked this photo"}
+  validates :user_id, presence: true, uniqueness: {scope: :photo_id, message: "already liked this photo"}
   validates :photo_id, :presence => true
 
 end
